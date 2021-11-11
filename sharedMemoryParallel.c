59203282782418d8438688e6e80b1a055b36ee6f
Freddie Jonas <fj6787@gmail.com>
2021-11-11 18:20:12 +0000

    Implementing proper precision mechanism

--- sharedMemoryParallel.c
+++ sharedMemoryParallel.c
@@ -4,20 +4,23 @@
 #include "pthread_barrier.c"
 
 int integerDimension = 4;
-double precision = 0.01;
+double precision = 0.001;
 double array[4][4] = {{1.0, 1.0, 1.0, 1.0}, {1.0, 0.0, 0.0, 0.0}, {1.0, 0.0, 0.0, 0.0}, {1.0, 0.0, 0.0, 0.0}};
 double newArray[4][4];
 int threadNumber = 0;
 int totalThreads = 0;
-pthread_mutex_t m;
+int precisionCount = 0;
+pthread_mutex_t threadNumberMutex;
+pthread_mutex_t precisionMutex;
 pthread_barrier_t barrier;
 
 void relaxationThread() {
     int localThreadNumber;
-    pthread_mutex_lock(&m);
+    int iterateOnce = 0;
+    pthread_mutex_lock(&threadNumberMutex);
     localThreadNumber = threadNumber;
     threadNumber++;
-    pthread_mutex_unlock(&m);
+    pthread_mutex_unlock(&threadNumberMutex);
     // barrier to ensure threads dont start iteration before all threads are created and thus loop variables wont cause errors
     pthread_barrier_wait(&barrier);
 
@@ -27,15 +30,22 @@ void relaxationThread() {
         for (int i = 1 + localThreadNumber; i < integerDimension - 1; i+= totalThreads) {
             for (int j = 1; j < integerDimension - 1; j++) {
                 result = (array[i-1][j] + array[i+1][j] + array[i][j-1] + array[i][j+1]) / 4;
-                // check if precision is reached
-                if (result - newArray[i][j] < precision && newArray[i][j] != 0.0)
+                // check if precision is reached for ALL values
+                if (precisionCount == (integerDimension-2)*(integerDimension-2))
                 {
                     // kill thread as finished
                     pthread_exit(NULL);
+                } else if (result - newArray[i][j] < precision && iterateOnce == 1) {
+                    // if precision not reached for ALL values, but is reached for this value then add 1 to the precisionCount
+                    precisionCount++;
+                } else {
+                    // if precision isn't reached then update the value and reset the precisionCount
+                    newArray[i][j] = result;
+                    precisionCount = 0;
                 }
-                newArray[i][j] = result;
             }
         }
+        iterateOnce = 1;
         // barrier to ensure threads dont start next iteration before all threads are finished
         pthread_barrier_wait(&barrier);
         // if the first thread then do the memory copy required
@@ -55,42 +65,20 @@ int main(void) {
     memcpy(newArray, array, sizeof(array));
 
     pthread_t threads[44];
-    pthread_mutex_init(&m, NULL);
+    pthread_mutex_init(&threadNumberMutex, NULL);
+    pthread_mutex_init(&precisionMutex, NULL);
+
     // number of threads is the number of rows to be operated on
     totalThreads = integerDimension - 2;
     pthread_barrier_init(&barrier, NULL, totalThreads);
-    printf("total threads %d\n", totalThreads);
     for (int x = 0; x < totalThreads; x++) {
-        //printf("%ld Thread to create\n", x);
         pthread_create(&threads[x], NULL, (void*(*)(void*))relaxationThread, NULL);
-        //printf("%ld thread success created\n", x);
     }
 
+    // join threads together so array will print once all finished
     for (int x = 0; x < 4; x++) {
         pthread_join(threads[x], NULL);
     }
-
-
-    // create new threads
-
-    // while (1) {
-    //     double result = 0.0;
-    //     // average 4 numbers around it
-    //     for (int i = 1; i < integerDimension - 1; i++) {
-    //         for (int j = 1; j < integerDimension - 1; j++) {
-    //             result = (array[i-1][j] + array[i+1][j] + array[i][j-1] + array[i][j+1]) / 4;
-    //             // check if precision is reached
-    //             if (result - newArray[i][j] < precision && newArray[i][j] != 0.0)
-    //             {
-    //                 goto printArray;
-    //             }
-    //             newArray[i][j] = result;
-    //         }
-    //     }
-    //     // do barrier here to wait for all threads to finish updating values
-    //     memcpy(array, newArray, sizeof(newArray));
-    //     // do barrier so threads dont start again until memory copy has finished
-    // }
 
     for (int i = 0; i < integerDimension; i++) {
         for (int j = 0; j < integerDimension; j++) {
